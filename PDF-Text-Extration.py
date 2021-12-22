f804a8fd0f0e7d192be3dd9b2c8b0326013db574
vincentwimmer <vincentwimmer@gmail.com>
2021-12-22 10:07:40 -0500

    Update PDF-Text-Extration.py

--- PDF-Text-Extration.py
+++ PDF-Text-Extration.py
@@ -1,1 +1,18 @@
-placeholder
+import pdfplumber
+import glob
+
+path = 'C:/Path/To/Work/Folder/'
+all_files = glob.glob(path + "*.pdf")
+
+textFilePath = (path + "outPut.txt")
+
+for pdfFileName in all_files:
+	pdfFile = pdfplumber.open(pdfFileName)
+	pdfPage = pdfFile.pages[0]
+	pdfText = pdfPage.extract_text()
+
+	input_file = open(textFilePath, "a")
+	input_file.write("\n" + pdfText + "\n")
+	input_file.close()
+
+	pdfFile.close()
