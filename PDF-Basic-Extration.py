89f825ae9f49d0d4dfa7783cd7a7297e20ca7467
vincentwimmer <vincentwimmer@gmail.com>
2021-12-22 12:02:36 -0500

    Update PDF-Basic-Extration.py

--- PDF-Basic-Extration.py
+++ PDF-Basic-Extration.py
@@ -1,20 +1,19 @@
 import pdfplumber
 import glob
 
-path = 'C:/Path/To/Work/Folder/'
+path = 'C:/Users/vwimmer/Documents/Git/Cooper/'
 all_files = glob.glob(path + "*.pdf")
 textFilePath = (path + "outPut.txt")
 
 # Iterate through all PDFs and dump text to outPut.txt
 for pdfFileName in all_files:
 	pdfFile = pdfplumber.open(pdfFileName)
-	pdfPage = pdfFile.pages[0]
-	pdfText = pdfPage.extract_text()
-
-	input_file = open(textFilePath, "a")
-	input_file.write("\n" + pdfText + "\n")
 	
-	input_file.close()
-	pdfFile.close()
+	for pdfPage in pdfFile.pages:
+		pdfText = pdfPage.extract_text()
+		input_file = open(textFilePath, "a")
+		input_file.write("\n" + pdfText + "\n")
+		input_file.close()
 
+	pdfFile.close()
 	print(pdfFileName, "Done.")
